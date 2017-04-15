2a7abfed43b93ef047d37b3d73cceb99c0b6d950
cookies4u <jcreyes.1018@gmail.com>
2017-04-14 20:32:21 -0700

    error handling updated

--- clozeDeleted.js
+++ clozeDeleted.js
@@ -8,15 +8,16 @@ function ClozeCard(text, cloze) {
 
     this.brokenCloze = text.indexOf(cloze);
 
-    //if (this.brokenCloze === -1 ) {throw new Error('Ooops, wrong phrase slected');}
-    // error checking return custom error
-    try {
-    	if(this.brokenCloze === -1)
-    		throw new Error("this suucks");
-    } catch(e) {
-    	//console.log(e.message);
-    	console.log(e.name+": "+e.message)
-    }
+    if (this.brokenCloze === -1 ) {throw new Error('Ooops, wrong phrase slected');}
+
+    // error checking return custom error // cathcing error when checking someone elses code
+    // try {
+    // 	if(this.brokenCloze === -1)
+    // 		throw new Error("this suucks");
+    // } catch(e) {
+    // 	//console.log(e.message);
+    // 	console.log(e.name+": "+e.message)
+    // }
 
   } else {
     return new ClozeCard(text, cloze);
